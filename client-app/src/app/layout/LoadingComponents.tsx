b2bc87b855458bcf07553a43564a0dddca0e6035
Andyyy228 <anton.bot1212@gmail.com>
2023-09-07 22:50:19 -0400

    End of section 7

--- client-app/src/app/layout/LoadingComponents.tsx
+++ client-app/src/app/layout/LoadingComponents.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Dimmer, Loader } from 'semantic-ui-react';
+import {Dimmer, Loader} from "semantic-ui-react";
 
 interface Props {
     inverted?: boolean;
-    content: string;
+    content?: string;
 }
 
 export default function LoadingComponent({inverted = true, content = 'Loading...'}: Props) {
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,8 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import 'semantic-ui-css/semantic.min.css'
-import './app/layout/styles.css'
-import App from './app/layout/App'
+import ReactDOM from "react-dom/client";
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
+import { StoreContext, store } from "./app/stores/store";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider> 
+  </React.StrictMode>
+);
